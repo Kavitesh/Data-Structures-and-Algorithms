6518ef34585cbe1a043373e8e1ac77b2485b6e9a
Kavitesh Kamboj <Kavitesh@users.noreply.github.com>
2022-06-17 15:17:41 +0000

    added merge sort

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const sort = require('./sorting/sort');
+const mergeSort = require('./sorting/mergeSort');
 
 
-// console.log(sort.bubbleSort(sort.numbers()));
-// console.log(sort.selectionSort(sort.numbers()));
+console.log(sort.bubbleSort(sort.numbers()));
+console.log(sort.selectionSort(sort.numbers()));
 console.log(sort.insertionSort(sort.numbers()));
+console.log(mergeSort.mergeSort(sort.numbers()));
--- sorting/sort.js
+++ sorting/sort.js
@@ -1,8 +1,8 @@
 const someNumbers = [199, 424, 61, 21, 31, 51, 63, 87, 283, 34, 0];
 
-const numbers = () =>someNumbers.map(n=>n);
+const numbers = () => someNumbers.map(n => n);
 
-const swap = ( array, i, j) =>{  
+const swap = (array, i, j) => {
   let temp = array[j]
   array[j] = array[i];
   array[i] = temp;
@@ -11,40 +11,43 @@ const swap = ( array, i, j) =>{
 const bubbleSort = (array) => {
   const length = array.length;
   for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length-i; j++) { // skip the last bubbled item by using -i
-      if(array[j] > array[j+1]) {
-        swap(array, j, j+1);
+    for (let j = 0; j < length - i; j++) { // skip the last bubbled item by using -i
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
       }
-    }        
+    }
+    // console.log("bubbled iteration", i + 1, array)
   }
   return array;
 }
 
 const selectionSort = (array) => {
-  for(let i = 0; i < array.length; i++){
+  for (let i = 0; i < array.length; i++) {
     let min = i;
-    for(let j = i+1; j < array.length; j++){ 
-      if (array[j] < array[min]){
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
         min = j;
       }
     }
     swap(array, i, min);
+    // console.log("selected", array[i], array)
   }
   return array;
 }
 
 const insertionSort = (array) => {
-  for(let i = 1; i < array.length; i++){
+  for (let i = 1; i < array.length; i++) {
     let curItem = array[i];
     let j;
-    for(j = i-1; (j >= 0 && curItem< array[j]); j--){
-      array[j+1]=array[j];
+    for (j = i - 1; (j >= 0 && curItem < array[j]); j--) {
+      array[j + 1] = array[j];
     }
-    array[j+1] = curItem;
+    array[j + 1] = curItem;
+    // console.log("inserted", curItem, array)
   }
   return array;
 }
 
 
 
-module.exports = {insertionSort, selectionSort, bubbleSort, numbers}
+module.exports = { insertionSort, selectionSort, bubbleSort, numbers }
